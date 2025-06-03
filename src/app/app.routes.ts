df6ce3b78d4dfe5c8825d0d8c6d019fe5c30b0fc
kalyan <kalyankatwal2@gmail.com>
2025-06-03 22:25:54 +0545

    App Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,17 @@ import { ReviewComponent } from './Admin/citizen/review/review.component';
 import { authGuard } from './auth.guard';
 import {CitizenFormComponent} from './citizen-form/citizen-form.component';
 import {CitizenViewComponent} from './Citizen/citizen-view/citizen-view.component';
+import {SuperAdminDashboardComponent} from './Admin/super-admin-dashboard/super-admin-dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'admin-dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'citizens', component: CitizenComponent, canActivate: [authGuard] },
   { path: 'citizen/review/:id', component: ReviewComponent, canActivate: [authGuard] },
   { path: 'citizen-view', component: CitizenViewComponent, canActivate: [authGuard] },
-  {path: 'citizen-form' ,component: CitizenFormComponent  }
+  {path: 'citizen-form' ,component: CitizenFormComponent  },
+  {path:'super-admin-dashboard', component: SuperAdminDashboardComponent}
+
 
 ];
