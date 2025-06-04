d2f6bb257ca966c6007eccff0b1e32fd8f434166
kalyan <kalyankatwal2@gmail.com>
2025-06-04 21:52:44 +0545

    Super Admin

--- src/app/Admin/super-admin-dashboard/super-admin-dashboard.component.ts
+++ src/app/Admin/super-admin-dashboard/super-admin-dashboard.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {DashboardComponent} from '../layout/dashboard/dashboard.component';
+import {NavbarComponent} from '../layout/navbar/navbar.component';
+import {SidebarComponent} from '../layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-super-admin-dashboard',
-  imports: [],
+  imports: [
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
   templateUrl: './super-admin-dashboard.component.html',
   styleUrl: './super-admin-dashboard.component.scss'
 })
