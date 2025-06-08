e32c19a7b5e3fb50c12ff54b51759ae5719091a8
kalyan <kalyankatwal2@gmail.com>
2025-06-08 10:10:06 +0545

    user login

---
+++ src/app/User/user-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = 'http://localhost:8080/api/v1/user';
+  // Check if email exists
+  checkEmailExists(email: string) {
+    return this.http.get<boolean>(`${this.baseUrl}/check?email=${email}`);
+  }
+  //find user by email
+  findUserByEmail(email: string | null) {
+    return this.http.get<any>(`${this.baseUrl}/by-email?email=${email}`);
+  }
+}
